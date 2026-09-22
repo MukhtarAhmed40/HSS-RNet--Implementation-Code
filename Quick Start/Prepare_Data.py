e277113e06b69f51c27906f695d2ae0e926a85b1
Ahmed <147239146+MukhtarAhmed40@users.noreply.github.com>
2026-09-22 22:12:21 +0800

    Add data.npz structure documentation in Prepare_Data.py

---
+++ Quick Start/Prepare_Data.py
@@ -0,0 +1,9 @@
+# data.npz
+{
+    "X_train": (N_train, D),  # Raw features
+    "y_train": (N_train,),     # Labels
+    "X_val": (N_val, D),
+    "y_val": (N_val,),
+    "X_test": (N_test, D),
+    "y_test": (N_test,),
+}
